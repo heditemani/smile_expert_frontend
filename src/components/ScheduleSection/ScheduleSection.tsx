ab4baa0341d4d11faac1e536229a69434e22d93e
heditemani <hedi.temani97@gmail.com>
2026-04-18 11:32:51 +0100

    start schedule section

---
+++ src/components/ScheduleSection/ScheduleSection.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './ScheduleSection.module.css';
+
+const ScheduleSection = () => {
+  return (
+    <section className={`${styles.scheduleSection}`}>
+        <div className="container">
+            
+
+        </div>
+    </section>
+  )
+}
+
+export default ScheduleSection
