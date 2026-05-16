ac6bff25a1fbf14b4961e5ed34dc7abc39819848
heditemani <hedi.temani97@gmail.com>
2026-05-16 13:10:40 +0100

    feat: add BeforeAfter section with detailed treatments and update Home page by removing unused ImageSlider component

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -3,7 +3,6 @@ import SecondBanner from '../../components/SecondBanner/SecondBanner'
 import ScheduleSection from '../../components/ScheduleSection/ScheduleSection'
 import bannerImg from '../../assets/images/contact_page/contact_banner1.webp';
 import CtaBanner from '../../components/CTAsection/CTA';
-import ImageSlider from '../../components/ImageSlider/ImageSlider.jsx'
 
 
 //  importation before after images 
@@ -21,12 +20,6 @@ const homepage = () => {
         buttonText={t('contact_hero.button')}
       />
     <ScheduleSection isAltBg={true} />
-
-    <ImageSlider title = "Dépigmentation Gingivale au Laser"
-                imageBefore = {beforeImage}
-                imageAfter = {afterImage} 
-                description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"/>
-
 
     <CtaBanner />
     </>
