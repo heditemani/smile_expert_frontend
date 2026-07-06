034084336035f8edfe28f3612a34edc2feb0adb5
heditemani <hedi.temani97@gmail.com>
2026-07-06 14:25:42 +0100

    Refactor code structure for improved readability and maintainability

--- src/Pages/SoinsBuccoDentaires/BuccoDentaire.tsx
+++ src/Pages/SoinsBuccoDentaires/BuccoDentaire.tsx
@@ -5,15 +5,25 @@ import { useTranslation } from 'react-i18next';
 
 import VideoExperience from '../../components/VideoExperience/VideoExperience.js';
 
-import facetimage_big from '../../assets/images/services/service1/facetimage_big.jpg';
-import facetimage1 from '../../assets/images/services/service1/facetimage1.jpg';
-import facetimage2 from '../../assets/images/services/service1/facetimage2.jpg';
+// Examens réguliers
+
+import examens_image_1 from '../../assets/images/soins bucco dentaire et prévntif/Examens réguliers/examens_image_1.webp';
+import examens_image_2 from '../../assets/images/soins bucco dentaire et prévntif/Examens réguliers/examens_image_2.webp';
+import examens_image_3 from '../../assets/images/soins bucco dentaire et prévntif/Examens réguliers/examens_image_3.webp';
+
+// Soins préventifs
+
+import soins_image_1 from '../../assets/images/soins bucco dentaire et prévntif/Soins préventifs/soins_image_1.webp';
+import soins_image_2 from '../../assets/images/soins bucco dentaire et prévntif/Soins préventifs/soins_image_2.webp';
+import soins_image_3 from '../../assets/images/soins bucco dentaire et prévntif/Soins préventifs/soins_image_3.webp';
+
+
 
 const BuccoDentaire = () => {
   const { t } = useTranslation();
 
   return (
-    <div className={Style.container}>
+    <div className={`${Style.container} ${Style.section_container}`}>
 
       <VideoExperience />
 
@@ -26,10 +36,10 @@ const BuccoDentaire = () => {
       <p className={Style.description}>
         {t('SoinsbuccoDentaire.Examensreguliers.description')}
       </p>
-      <img className={Style.image_big} src={facetimage_big} alt={t('SoinsbuccoDentaire.Examensreguliers.title')} />
+      <img className={Style.image_big} src={examens_image_1} alt={t('SoinsbuccoDentaire.Examensreguliers.title')} />
       <div className={Style.images_container}>
-        <img src={facetimage1} alt={t('SoinsbuccoDentaire.Examensreguliers.title')} />
-        <img src={facetimage2} alt={t('SoinsbuccoDentaire.Examensreguliers.title')} />
+        <img src={examens_image_2} alt={t('SoinsbuccoDentaire.Examensreguliers.title')} />
+        <img src={examens_image_3} alt={t('SoinsbuccoDentaire.Examensreguliers.title')} />
       </div>
 
       <SectionTitle 
@@ -40,10 +50,10 @@ const BuccoDentaire = () => {
       <p className={Style.description}>
         {t('SoinsbuccoDentaire.SoinsPreventifs.description')}
       </p>
-      <img className={Style.image_big} src={facetimage_big} alt={t('SoinsbuccoDentaire.SoinsPreventifs.title')} />
+      <img className={Style.image_big} src={soins_image_1} alt={t('SoinsbuccoDentaire.SoinsPreventifs.title')} />
       <div className={Style.images_container}>
-        <img src={facetimage1} alt={t('SoinsbuccoDentaire.SoinsPreventifs.title')} />
-        <img src={facetimage2} alt={t('SoinsbuccoDentaire.SoinsPreventifs.title')} />
+        <img src={soins_image_2} alt={t('SoinsbuccoDentaire.SoinsPreventifs.title')} />
+        <img src={soins_image_3} alt={t('SoinsbuccoDentaire.SoinsPreventifs.title')} />
       </div>
     </div>
       
